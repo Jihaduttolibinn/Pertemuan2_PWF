f0b2c44eb3026de4cf03d0f580ef0fc7390623a0
jihaduttolibinn <jihaduttolibin02@gmail.com>
2026-03-14 22:33:29 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,6 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/about', [AboutController::class, 'index'])->name('about');
+    Route::resource('product', ProductController::class);
 });
 
 require __DIR__.'/auth.php';
